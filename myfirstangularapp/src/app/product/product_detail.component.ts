71d9512b7ac6e94dc83960f97eb3d67273298e13
varsha Surnar <varshasurnar@gmail.com>
2019-01-27 16:27:42 +0530

    call API  Param

--- myfirstangularapp/src/app/product/product_detail.component.ts
+++ myfirstangularapp/src/app/product/product_detail.component.ts
@@ -1,21 +1,40 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import { ProductService } from './product.service';
+import { IProduct } from './product.model';
+
 
 @Component({
     templateUrl : './product_detail.component.html',
 })
 
 export class ProductDetailComponent implements OnInit{
-    name : string;
+    id : number;
+    details : IProduct[];
+
+    constructor(private _route : ActivatedRoute,
+                private _prodServ : ProductService,
+                private _router : Router){}
+      
+      ngOnInit(): void {
+            this.id = this._route.snapshot.params['id'];
+            this._prodServ.getProductDetail(this.id)
+                .subscribe((data) => this.details = data);
+        }
+
+        OnBack() : void {
+            this._router.navigate(['products']);
+        }
+
+    
+}
+
+/*
+name : string;
     desc : string;
     imgUrl : string;
-
-    constructor(private _route : ActivatedRoute){}
-
-    ngOnInit() : void {
-        this._route.queryParams.subscribe((data) => {this.name = data['pname'];
+ this._route.queryParams.subscribe((data) => {this.name = data['pname'];
                                                     this.imgUrl = data['pimg'];
                                                     this.desc = data['pdesc']
                                             })
-    }
-}
+*/
